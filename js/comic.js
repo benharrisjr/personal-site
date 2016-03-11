6f34a0dd75d5974c5fff986617f1ce3d862599b2
Benjamin Harris Jr <benharrisjr+sourcetree@gmail.com>
2016-03-10 21:59:34 -0500

    Set up initial folder structure

---
+++ js/comic.js
@@ -0,0 +1,1 @@
+//Default comic size 1000x600 or 600x1000
---
+++ js/main.js
@@ -0,0 +1,13 @@
+var websites = document.getElementById('websites');
+var games = document.getElementById('games');
+var apps = document.getElementById('apps');
+var software = document.getElementById('software');
+var things = document.getElementById('websites');
+websites.onclick = ShowSection()
+
+var spinnerSections = document.getElementsByClassName("c-show-section");
+spinnerSections.onclick = ShowSection();
+
+function ShowSection(){
+	
+}
